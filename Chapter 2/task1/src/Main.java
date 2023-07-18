db7754aa51e8afea9e47526580706dfb0247690e
k4rtowka <k4rtowkayt@gmail.com>
2023-07-18 17:09:16 +0300

    add solution
    add the solution to the first exercise

--- Chapter 2/task1/src/Main.java
+++ Chapter 2/task1/src/Main.java
@@ -1,5 +1,18 @@
+//:src/Main.java
+
+/** First exercise from book
+ * @author k4rtowka
+ */
 public class Main {
+    /**
+     * Start-point
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Test test = new Test();
+        System.out.println(test.i);
+        System.out.println(test.c);
     }
-}
+}/*  Output: 0
+null
+*///:~
